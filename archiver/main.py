465c22829ebb8ad808b3914f12047837896a7dcf
ddumitrache <ddumitrache@gocardless.com>
2022-01-06 08:56:47 +0000

    Work on CI

--- archiver/main.py
+++ archiver/main.py
@@ -1,7 +1,6 @@
 import os
 
 import click
-from zen_core.configuration.config_reader import read_config
 from zen_core.handlers.git_client import GitClient
 from zen_core.logging.printer import write, focus_out
 from zen_core.parsing.toml_parser import parse_repo_configuration
@@ -12,38 +11,12 @@ def apply_configuration(git_client, repo_configurations, dry_run):
         matching_repos = git_client.search_repos(f'git-sentry/{repo_name}')
 
         for repo in matching_repos:
-            repo.archive()
-        for repo in matching_repos:
-            print(f'{repo.full_name()} - {repo.archived()}')
-            # for org in matching_orgs:
-            #     org_config = org_config.resolve(org)
-            #     current_configuration = org.configuration()
-            #
-            #     difference = org_config.diff(current_configuration)
-            #
-            #     if difference.length() != 0:
-            #         write(f'Configuration analysis for {focus_out(org.login())} is complete.')
-            #         write(difference)
-            #
-            #     if not dry_run:
-            #         for m in difference.members():
-            #             org.grant_access(m)
-            #
-            #         for m in difference.admins():
-            #             org.grant_access(m, role='admin')
-            #
-            #         for team_name, team_config in difference.teams().items():
-            #             existing_team = org.team(team_name)
-            #             if existing_team is None:
-            #                 existing_team = org.create_team(team_name)
-            #
-            #             for repo, permission in team_config.repos().items():
-            #                 existing_team.add_to_repo(repo, permission)
-            #
-            #             for member in team_config.members():
-            #                 existing_team.grant_access(member)
-            #             for admin in team_config.admins():
-            #                 existing_team.grant_access(admin, role='maintainer')
+            if dry_run:
+                if not repo.archived():
+                    print(f'{repo.full_name()}: {repo.archived()} -> True')
+            else:
+                repo.archive()
+                print(f'{repo.full_name()} - {repo.archived()}')
 
 
 @click.group()
